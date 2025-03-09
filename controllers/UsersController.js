502200ed2a13880cd94b25d9176dc0e90a543712
uitxmic <157990036+uitxmic@users.noreply.github.com>
2025-03-09 22:13:38 +0700

    update write api with async await

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,35 +1,40 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 class UsersController
 {
     constructor()
     {
-        this.connection = mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
-        });
+        this.initConnection();
+    }
 
-        this.connection.connect((err) => {
-            if (err) {
-                console.error('Error connecting to the database:', err);
-                return;
-            }
-            console.log('Connected to the database');
-        });
+    async initConnection() 
+    {
+        try {
+            this.connection = await mysql.createConnection({
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_NAME
+            });
+
+            console.log('Connected to the database (async)');
+        } catch (err) {
+            console.error('Database connection error:', err);
+        }
     }
     // [Get] /users
-    GetAllUser = (req, res) =>
+    GetAllUser = async (req, res) =>
     {
-        this.connection.query('SELECT * FROM Users', (error, results, fields) => {
-            if (error) {
-                res.status(500).json({ error: 'Database query error' });
-                return;
-            }
-            res.json(results);
-        });
+        try 
+        {
+            const [results] = await this.connection.query('CALL get_all_users()');
+            res.json(results[0]); // Chỉ trả về kết quả SELECT
+        } 
+        catch (error) {
+            console.error('Query error:', error);
+            res.status(500).json({ error: 'Database query error', details: error.message });
+        }
     }
 }
 

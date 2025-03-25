b6ac8143151de168ca576e007cc29c03de4857ee
uitxmic <157990036+uitxmic@users.noreply.github.com>
2025-03-26 01:53:15 +0700

    Update route for trade

--- routes/trade.js
+++ routes/trade.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const tradeController = require('../controllers/TradeController');
 
-router.use('/', tradeController.Get20LastestPosts);
+router.use(authMiddleware);
+
+router.use('/get20LatestPosts', tradeController.Get20LastestPosts);
 
 module.exports = router;

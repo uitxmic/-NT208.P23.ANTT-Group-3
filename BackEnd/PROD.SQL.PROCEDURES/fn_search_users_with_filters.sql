3b490419826d24f5d62987a621a400eaedf16b72
Whats-up-pro <23521308@gm.uit.edu.vn>
2025-04-29 16:01:11 +0700

    modified:   BackEnd/PROD.SQL.PROCEDURES/fn_search_users_with_filters.sql

--- BackEnd/PROD.SQL.PROCEDURES/fn_search_users_with_filters.sql
+++ BackEnd/PROD.SQL.PROCEDURES/fn_search_users_with_filters.sql
@@ -10,29 +10,27 @@ CREATE PROCEDURE `fn_search_users_with_filters`(
 )
 BEGIN
     SELECT 
-        u.UserId,
-        u.Username,
-        u.Fullname,
-        u.Email,
-        u.PhoneNumber,
-        u.AvgRate,
-        u.IsVerified,
-        u.SoldAmount
-    FROM User u
-    LEFT JOIN Feedback f ON u.UserId = f.UserId
+        UserId,
+        Username,
+        Fullname,
+        Email,
+        PhoneNumber,
+        AvgRate,
+        SoldAmount
+    FROM User 
     WHERE 
-        (u.Username LIKE CONCAT('%', search_term, '%') 
-        OR u.Fullname LIKE CONCAT('%', search_term, '%') 
-        OR u.Email LIKE CONCAT('%', search_term, '%'))
-        AND (u.AvgRate >= min_feedback OR min_feedback IS NULL)
-        AND (u.SoldAmount >= min_sold OR min_sold IS NULL)
+        (Username LIKE CONCAT('%', search_term, '%') 
+        OR Fullname LIKE CONCAT('%', search_term, '%') 
+        OR Email LIKE CONCAT('%', search_term, '%'))
+        AND (AvgRate >= min_feedback OR min_feedback IS NULL)
+        AND (SoldAmount >= min_sold OR min_sold IS NULL)
     ORDER BY 
         CASE 
-            WHEN sort_by = 'feedback_desc' THEN u.AvgRate
-            WHEN sort_by = 'feedback_asc' THEN -u.AvgRate
-            WHEN sort_by = 'sold_desc' THEN u.SoldAmount
-            WHEN sort_by = 'sold_asc' THEN -u.SoldAmount
-            ELSE u.UserId -- Mặc định sắp xếp theo UserId nếu sort_by không hợp lệ
+            WHEN sort_by = 'feedback_desc' THEN -AvgRate
+            WHEN sort_by = 'feedback_asc' THEN AvgRate
+            WHEN sort_by = 'sold_desc' THEN -SoldAmount
+            WHEN sort_by = 'sold_asc' THEN SoldAmount
+            ELSE UserId -- Mặc định sắp xếp theo UserId nếu sort_by không hợp lệ
         END;
 END$$
 
